68af3e35eb5dec87f95c01cff3c0e0139d5ee141
Osama Belal <obelal010@gmail.com>
2022-12-31 17:52:58 +0200

    Update register screen

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserDTO } from '../DTO/UserDTO';
+import { UserService } from '../Service/User/user.service';
 
 @Component({
   selector: 'app-register',
@@ -15,14 +17,34 @@ import { FormControl, Validators } from '@angular/forms';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export default class RegisterComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
+
+  constructor(public userService: UserService) { }
+
   hidePassword = true
-  
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+  important = "Must Enter a Value!"
+  mailValid = "Must Enter a Valid Email!"
+  limitLength = "15 Characters Only!"
+
+  userGroup = new FormGroup({
+    userName: new FormControl('', [Validators.required, Validators.maxLength(15)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+  userError(field: string){ return this.userGroup.get(field)?.invalid }
+  userErrorMessage(field: string){ 
+    return this.userGroup.get(field)?.hasError('required') ? this.important : 
+    this.userGroup.get(field)?.hasError('email') ? this.mailValid : 
+    this.userGroup.get(field)?.hasError('maxlength') ? this.limitLength : '';
+  }
+
+  submitUser(){
+    let user = <UserDTO>{
+      userName: this.userGroup.controls.userName.value,
+      email: this.userGroup.controls.email.value,
+      password: this.userGroup.controls.password.value
     }
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    this.userService.Create(user);
   }
 }
